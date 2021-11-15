b42aadd397440a02574bf5af63c0c7828f45dcd9
wonjoon <39115630+wnjoon@users.noreply.github.com>
2021-11-16 06:31:46 +0900

    init

--- payload/introduce.ts
+++ payload/introduce.ts
@@ -5,11 +5,11 @@ const introduce: IIntroduce.Payload = {
   disable: false,
 
   contents: [
-    'Have various experience in security, cryptographic and blockchain. Interested in developing a user-friendly service platform and token economy by digital currency.',
+    '3년간의 임상간호사 경험 이후 현재까지 6년간 석유화학공업단지와 LG사이언스파크, 그리고 현재 한국산업안전보건공단에 이르기까지 다양한 환경에서 보건관리자 업무를 수행하고 있습니다.',
+    '최근에는 사업장 보건관리 뿐만 아니라 안전, 건설 분야 등 산업안전보건법 기반의 다양한 정책사업을 이해하고 많은 회사들을 지원 및 관리하고 있습니다',
     '',
-    '보안, 암호학 및 블록체인까지 다양한 경험을 갖고 있습니다. 사용자 중심의 서비스 플랫폼 개발에 관심이 있으며, 최근에는 디지털화폐를 이용한 토큰 이코노미의 변화에 가장 관심이 많습니다.',
   ],
-  sign: 'Wonjoon Lee',
+  sign: 'Kyuyoung Choi',
   // sign: packageJson.author.name,
   // latestUpdated: '2019-07-24',
   latestUpdated: lastestUpdatedAt,
